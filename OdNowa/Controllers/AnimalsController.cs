cf7c37419bd1cdc16712e2d5f739669cb9f4502d
Łukasz Grzybowski <s18445@pjwstk.edu.pl>
2020-04-25 17:21:19 +0200

    1

---
+++ OdNowa/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdNowa.Controllers
+{
+    [Route("api/animals]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAnimals()
+        {
+            return "Julek, Rudy, Krowa";
+        }
+    }
+}
