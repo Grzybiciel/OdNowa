359c28ea3b41e541d63a25b50b85e310209f667a
Łukasz Grzybowski <s18445@pjwstk.edu.pl>
2020-04-25 18:54:42 +0200

    3. zadanie pierwsze w pełni zrobione

--- OdNowa/Controllers/AnimalsController.cs
+++ OdNowa/Controllers/AnimalsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -11,10 +12,95 @@ namespace OdNowa.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
-        [HttpGet]
-        public string GetAnimals()
+   /*     [HttpGet("{sortBy}")]//poprzez URL
+        public IActionResult GetAnimals(string sortBy)
         {
-            return "Julek, Rudy, Krowa";
+            var sort1 = $"{sortBy}";
+            var sort = sort1.ToLower();
+            if (sort == "")
+            {
+                sort = "AdmissionDate";
+            }
+            else if (sort != "admissiondate" & sort != "name" & sort != "type" & sort != "lastname")
+            {
+                return BadRequest("Niepoprawny parametr");
+            }
+
+            List<Object> listQ = new List<Object>();
+            using (var client = new SqlConnection("Data Source = db-mssql; Initial Catalog = s18445; Integrated Security = True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = client;
+                com.CommandText = $"SELECT Name, Type, AdmissionDate, LastName FROM Animal, Owner WHERE Animal.IdOwner = Owner.IdOwner ORDER BY {sort}";
+                client.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var animal = new
+                    {
+                        Name = dr["Name"].ToString(),
+                        Type = dr["Type"].ToString(),
+                        AdmissionDate = DateTime.Parse(dr["AdmissionDate"].ToString()),
+                        LastName = dr["LastName"].ToString()
+                    };
+                    listQ.Add(animal);
+                }
+            }
+            if (listQ.Count != 0)
+            {
+                return Ok(listQ);
+            }
+            else
+            {
+                return NotFound("Animal not Found");
+            }
+
         }
+        */
+                [HttpGet]
+                public IActionResult GetAnimals(string sortBy)//poprzez queryString
+                {
+                    var sort1 = $"{sortBy}";
+                    var sort = sort1.ToLower();
+                    if(sort =="")
+                    {
+                        sort = "AdmissionDate";
+                    }
+                    else if (sort != "admissiondate" & sort!="name" & sort!="type" &sort!="lastname")
+                    {
+                        return BadRequest("Niepoprawny parametr");
+                    }
+
+                    List<Object> listQ = new List<Object>();
+                    using (var client = new SqlConnection("Data Source = db-mssql; Initial Catalog = s18445; Integrated Security = True"))
+                    using (var com = new SqlCommand())
+                    {
+                        com.Connection = client;
+                        com.CommandText = $"SELECT Name, Type, AdmissionDate, LastName FROM Animal, Owner WHERE Animal.IdOwner = Owner.IdOwner ORDER BY {sort}";
+                        client.Open();
+                        var dr = com.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            var animal = new
+                            {
+                                Name = dr["Name"].ToString(),
+                                Type = dr["Type"].ToString(),
+                                AdmissionDate = DateTime.Parse(dr["AdmissionDate"].ToString()),
+                                LastName = dr["LastName"].ToString()
+                            };
+                            listQ.Add(animal);
+                        }
+                    }
+                    if (listQ.Count != 0)
+                    {
+                        return Ok(listQ);
+                    }
+                    else
+                    { 
+                        return NotFound("Animal not Found");
+                    }
+
+                }
+            
     }
 }
