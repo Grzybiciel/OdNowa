53696deab36ec3ab34f49801aa32ee9a0c3744a4
Łukasz Grzybowski <s18445@pjwstk.edu.pl>
2020-04-25 20:44:26 +0200

    4. Zmiana Animal na GetAnimalRequest i dodanie GetAnimalResponse

--- OdNowa/Controllers/AnimalsController.cs
+++ OdNowa/Controllers/AnimalsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OdNowa.Models;
 
 namespace OdNowa.Controllers
 {
@@ -70,8 +71,7 @@ namespace OdNowa.Controllers
                     {
                         return BadRequest("Niepoprawny parametr");
                     }
-
-                    List<Object> listQ = new List<Object>();
+                    GetAnimalResponse response=null;
                     using (var client = new SqlConnection("Data Source = db-mssql; Initial Catalog = s18445; Integrated Security = True"))
                     using (var com = new SqlCommand())
                     {
@@ -81,19 +81,20 @@ namespace OdNowa.Controllers
                         var dr = com.ExecuteReader();
                         while (dr.Read())
                         {
-                            var animal = new
+                            GetAnimalRequest animal = new GetAnimalRequest();  
                             {
-                                Name = dr["Name"].ToString(),
-                                Type = dr["Type"].ToString(),
-                                AdmissionDate = DateTime.Parse(dr["AdmissionDate"].ToString()),
-                                LastName = dr["LastName"].ToString()
+                            animal.Name = dr["Name"].ToString();
+                            animal.Type = dr["Type"].ToString();
+                            animal.AdmissionDate = DateTime.Parse(dr["AdmissionDate"].ToString());
+                            animal.LastName = dr["LastName"].ToString();
                             };
-                            listQ.Add(animal);
+                            response.AnimalsList.Add(animal);
                         }
                     }
-                    if (listQ.Count != 0)
+                   
+                    if (response.AnimalsList.Count != 0)
                     {
-                        return Ok(listQ);
+                        return Ok(response);
                     }
                     else
                     { 
@@ -101,6 +102,40 @@ namespace OdNowa.Controllers
                     }
 
                 }
-            
+
+        /*  [HttpPost]
+          public IActionResult AddAnimal(AnimalRequest animal)
+          {
+              List<Object> listQ = new List<Object>();
+              using (var client = new SqlConnection("Data Source = db-mssql; Initial Catalog = s18445; Integrated Security = True"))
+              using (var com = new SqlCommand())
+              {
+                  com.Connection = client;
+                  com.CommandText = $"SELECT Name, Type, AdmissionDate, LastName FROM Animal, Owner WHERE Animal.IdOwner = Owner.IdOwner ORDER BY {sort}";
+                  client.Open();
+                  var dr = com.ExecuteReader();
+                  while (dr.Read())
+                  {
+                      var animal = new
+                      {
+                          Name = dr["Name"].ToString(),
+                          Type = dr["Type"].ToString(),
+                          AdmissionDate = DateTime.Parse(dr["AdmissionDate"].ToString()),
+                          LastName = dr["LastName"].ToString()
+                      };
+                      listQ.Add(animal);
+                  }
+              }
+              if (listQ.Count != 0)
+              {
+                  return Ok(listQ);
+              }
+              else
+              {
+                  return NotFound("Animal not Found");
+              }
+
+          }
+          */
     }
 }
--- OdNowa/DTO/Requests/GetAnimalRequest.cs
+++ OdNowa/DTO/Requests/GetAnimalRequest.cs
@@ -5,12 +5,11 @@ using System.Threading.Tasks;
 
 namespace OdNowa.Models
 {
-    public class Animal
+    public class GetAnimalRequest
     {
-        public int IdAnimal { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public DateTime AdmissionDate { get; set; }
-        public int IdOwner { get; set; }
+        public string LastName { get; set; }
     }
 }
